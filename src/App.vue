e6c3501ba3757dda2aba947fd1ac58800f8069d4
serena-peschiaroli <serena.peschiaroli@gmail.com>
2024-02-01 16:28:11 +0100

    Add: AppMain component; axios request in appmain component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,31 @@
 <script>
+import AppMain from './components/AppMain.vue';
+
+export default {
+  data() {
+    return{
+
+    }
+  },
+  components: { 
+      AppMain,
+  }
+}
+
+
 </script>
 
 
 
 <template>
+  <div id="app">
+    <AppMain/>
+  </div>
 
 </template>
 
 <style lang="scss">
 
-@use ".styles/general.scss";
+@use "./styles/general.scss";
 
 </style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,28 @@
+<script>
+import axios from 'axios';
+export default{
+    data(){
+        return{
+            baseUrl: 'http://127.0.0.1:8000',
+            projects: [],
+        }
+    },
+    created() {
+        axios.get(`${this.baseUrl}/api/projects`)
+        .then((resp)=> {
+            
+            this.projects = resp.data.results.data;
+            console.log(resp.data.results.data);
+        });
+    }
+}
+</script>
+
+<template>
+    <h1>test</h1>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
